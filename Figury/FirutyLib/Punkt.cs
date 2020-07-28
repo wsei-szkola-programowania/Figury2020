20eb1b7a49cd74b8b462e4acb276a71cbea2a9bc
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2020-07-28 20:21:52 +0200

    Punkt

---
+++ Figury/FirutyLib/Punkt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FirutyLib
+{
+    public class Punkt
+    {
+        #region stan -> pola, właściwości
+        double x;
+        double y;
+        #endregion
+
+        #region konstrukcja -> konstruktory
+        Punkt() // konstruktor bezparametrowy, domyślny
+        {
+            x = y = 0;            
+        }
+
+        Punkt( double x, double y )
+        {
+            this.x = x;
+            this.y = y;
+        }
+        #endregion
+
+
+        #region zachowanie -> właściwości, metody
+
+        void Przesun(double dx, double dy)
+        {
+            x = x + dx; // x += dx
+            y = y + dy; // y += dy
+        }
+
+        #endregion
+    }
+}
