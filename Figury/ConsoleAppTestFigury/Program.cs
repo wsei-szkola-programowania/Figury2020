89c1a00d031acad1b1d3c3de29cd1cf1141f20ff
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2020-07-29 18:36:24 +0200

    Punkt

---
+++ Figury/ConsoleAppTestFigury/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FirutyLib;
+
+namespace ConsoleAppTestFigury
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Punkt a = new Punkt();
+            Console.WriteLine( a.ToString() );
+            a.Przesun(dx: +1, dy: -1);
+            Console.WriteLine( a );
+            Punkt b = a;
+            Console.WriteLine(a == b);
+            b.Przesun(-1, 1);
+            Console.WriteLine(a == b);
+            Console.WriteLine( b );
+            Punkt c = new Punkt(0, 0);
+            Console.WriteLine(b == c); // referencyjne równa się
+            Console.WriteLine( b.Equals(c) ); // równość Equals
+
+        }
+    }
+}
--- Figury/FirutyLib/Punkt.cs
+++ Figury/FirutyLib/Punkt.cs
@@ -2,34 +2,61 @@
 
 namespace FirutyLib
 {
-    public class Punkt
+    public class Punkt : IEquatable<Punkt>
     {
         #region stan -> pola, właściwości
-        double x;
-        double y;
+        public double x;
+        public double y;
         #endregion
 
         #region konstrukcja -> konstruktory
-        Punkt() // konstruktor bezparametrowy, domyślny
+        public Punkt() // konstruktor bezparametrowy, domyślny
         {
             x = y = 0;            
         }
 
-        Punkt( double x, double y )
+        public Punkt( double x, double y )
         {
             this.x = x;
             this.y = y;
         }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Punkt)
+                return this.Equals((Punkt)obj);
+            else
+                return false;
+        }
+
+
+        public bool Equals(Punkt other)
+        {
+            if (other == null) return false;
+
+            if (this.x == other.x && this.y == other.y)
+                return true;
+            else
+                return false;            
+        }
+
         #endregion
 
 
         #region zachowanie -> właściwości, metody
 
-        void Przesun(double dx, double dy)
+        public void Przesun(double dx, double dy)
         {
             x = x + dx; // x += dx
             y = y + dy; // y += dy
         }
+
+        public override string ToString()
+        {
+            return $"Punkt({x}, {y})";
+        }
+
+        
 
         #endregion
     }
